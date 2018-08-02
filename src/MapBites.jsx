54c0795afe099e8c9d98597b9fa9d8048da5abc2
SJohnson <simonbjohnson@gmail.com>
2018-08-02 15:06:44 +0100

    New map component

--- src/MapBites.jsx
+++ src/MapBites.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SimpleMap from './simple-map.jsx';
+import SimpleMap from './simple-leaflet-geojson.jsx';
 import * as topojson from 'topojson';
 import axios from 'axios';
 
@@ -14,7 +14,7 @@ class MapBites extends Component {
         map.bite.forEach(function(d,i){
           codes[d[0]] = d[1];
         });
-        bites.push(<MapBite key={key} url={map.geom_url} title={map.title} codes={codes} attribute={map.geom_attribute} id={map.id} uniqueID={map.uniqueID} />);
+        bites.push(<MapBite key={key} uniqueKey={key} url={map.geom_url} title={map.title} codes={codes} attribute={map.geom_attribute} id={map.id} uniqueID={map.uniqueID} />);
       });
 
       return (
@@ -27,24 +27,37 @@ class MapBite extends Component {
 
   constructor(props){
       super(props)
-      this.state = { loading: true, geom: null};
-      this.loadGeom(this.props.url);
+      this.state = { loading: true, geom: []};
+      this.loadGeoms(this.props.url);
   }
 
-  componentWillReceiveProps(nextProps) {
+  /*componentWillReceiveProps(nextProps) {
     this.state = { loading: true, geom: null};
-    this.loadGeom(nextProps.url);
-  }
+    this.loadGeoms(nextProps.url);
+  }*/
 
-  loadGeom(url){
-    let self = this;
+  loadGeoms(urls){
+    var self = this;
+    if(urls.length>0){
 
-    axios
-      .get(url)
-      .then(function(result) {
-        let geom = topojson.feature(result.data,result.data.objects.geom);
-        self.setState({geom: geom, loading: false});
-      });
+      axios
+        .get(urls[0])
+        .then(function(result) {
+          var geom = {};
+          if(result.data.type=='Topology'){
+            geom = topojson.feature(result.data,result.data.objects.geom);
+          } else {
+            geom = result.data;
+          }
+          self.setState({geom: self.state.geom.concat(geom)},
+            function(){
+              self.loadGeoms(urls.slice(1));
+            }
+          );
+        });
+    } else {
+      self.setState({loading: false});
+    }
   }
 
   render(){
@@ -54,7 +67,7 @@ class MapBite extends Component {
     if(this.state.loading){
       comp = <h1>Loading Map</h1>;
     } else {
-      comp = <SimpleMap width="350" height="400" geom={this.state.geom} colors={colors} codes={this.props.codes} attribute={this.props.attribute} />;
+      comp = <SimpleMap uniqueKey={this.props.uniqueKey} width="350" height="400" geom={this.state.geom} colors={colors} codes={this.props.codes} attribute={this.props.attribute} />;
     }
     return (
       <div className="mapbite">{this.props.title} - {this.props.id} - {this.props.uniqueID}<div>{comp}</div></div>
